423219bce448d8a64a0f4ea87be30c4a9da093c3
Jaskeerat <jaskeeratsingh219@gmail.com>
2024-10-22 00:09:02 +0530

    structuring

---
+++ Folder/Array/callRef.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+}
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    inc(arr, size);
+    printArray(arr, size) return 0;
+}
