cdad4ae1b888d811aac7ad932d2d4df3c4836de0
Jaskeerat <jaskeeratsingh219@gmail.com>
2024-11-05 17:03:55 +0530

    call by ref

--- Folder/Array/callRef.cpp
+++ Folder/Array/callRef.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
-
 void printArray(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void inc(int arr[], int size)
+{
+    arr[0] = arr[0] + 10;
+    arr[1] = arr[1] - 1;
+    printArray(arr, size);
 }
 int main()
 {
-    int size;
-    cin >> size;
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        cin >> arr[i];
-    }
+    int arr[] = {1, 2};
+    int size = 2;
     inc(arr, size);
-    printArray(arr, size) return 0;
+    printArray(arr, size);
+    return 0;
 }
